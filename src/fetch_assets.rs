bd7464ea38715aba486bd5704b9ef6bad32d7ea1
hyoi <hyoi.kaz@gmail.com>
2022-01-22 16:14:06 +0900

    ソースコードのマイナーな変更

--- src/fetch_assets.rs
+++ src/fetch_assets.rs
@@ -21,7 +21,6 @@ impl Plugin for PluginFetchAssets
 		)
 		.add_system_set													// ＜GameState::Init＞
 		(	SystemSet::on_exit( GameState::Init )						// ＜on_exit()＞
-//				.with_system( spawn_text_ui_message )					// プリロード後にUIを生成
 				.with_system( despawn_entity::<PreloadingAnimeTile> )	// スプライトの削除
 		)
 		//------------------------------------------------------------------------------------------
@@ -30,8 +29,6 @@ impl Plugin for PluginFetchAssets
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//定義と定数
 
 //ロードしたAssetのハンドルの保存先
 struct LoadedAssets { preload: Vec<HandleUntyped> }
@@ -145,134 +142,14 @@ fn move_preload_anime_tile
 
 //ローディングアニメ用スプライトのバンドルを生成
 fn sprite_preloading_anime_tile( ( x, y ): ( f32, f32 ) ) -> SpriteBundle
-{	let position  = Vec3::new( x, y, SPRITE_TILE_DEPTH );
-	let square    = Vec2::new( SPRITE_TILE_PIXEL, SPRITE_TILE_PIXEL );
-	let transform = Transform::from_translation( position );
-	let sprite    = Sprite
+{	let sprite = Sprite
 	{	color: SPRITE_TILE_COLOR,
-		custom_size: Some( square ),
+		custom_size: Some( Vec2::new( SPRITE_TILE_PIXEL, SPRITE_TILE_PIXEL ) ),
 		..Default::default()
 	};
+	let transform = Transform::from_translation( Vec3::new( x, y, SPRITE_TILE_DEPTH ) );
 
-	SpriteBundle { transform, sprite, ..Default::default() }
+	SpriteBundle { sprite, transform, ..Default::default() }
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-/*
-//テキストUIを配置する
-pub fn spawn_text_ui_message( mut cmds: Commands, asset_svr: Res<AssetServer> )
-{	//中央に表示するtext
-	let mut demo_text  = text_ui( &MESSAGE_DEMO , &asset_svr );
-	let mut start_text = text_ui( &MESSAGE_START, &asset_svr );
-	let mut clear_text = text_ui( &MESSAGE_CLEAR, &asset_svr );
-	let mut over_text  = text_ui( &MESSAGE_OVER , &asset_svr );
-	let mut pause_text = text_ui( &MESSAGE_PAUSE, &asset_svr );
-
-	demo_text.visibility.is_visible  = false;
-	start_text.visibility.is_visible = false;
-	clear_text.visibility.is_visible = false;
-	over_text.visibility.is_visible  = false;
-	pause_text.visibility.is_visible = false;
-
-	//上端・下端に表示するtext
-	let mut ui_upper_left   = text_ui( &UI_UPPER_LEFT  , &asset_svr );
-	let mut ui_lower_left   = text_ui( &UI_LOWER_LEFT  , &asset_svr );
-	let mut ui_upper_center = text_ui( &UI_UPPER_CENTER, &asset_svr );
-	let mut ui_lower_center = text_ui( &UI_LOWER_CENTER, &asset_svr );
-	let mut ui_upper_right  = text_ui( &UI_UPPER_RIGHT , &asset_svr );
-	let mut ui_lower_right  = text_ui( &UI_LOWER_RIGHT , &asset_svr );
-
-	ui_upper_left.style.align_self   = AlignSelf::FlexStart;
-	ui_lower_left.style.align_self   = AlignSelf::FlexStart;
-	ui_upper_center.style.align_self = AlignSelf::Center;
-	ui_lower_center.style.align_self = AlignSelf::Center;
-	ui_upper_right.style.align_self  = AlignSelf::FlexEnd;
-	ui_lower_right.style.align_self  = AlignSelf::FlexEnd;
-
-	ui_upper_left.text.alignment.horizontal   = HorizontalAlign::Left;
-	ui_lower_left.text.alignment.horizontal   = HorizontalAlign::Left;
-	ui_upper_center.text.alignment.horizontal = HorizontalAlign::Center;
-	ui_lower_center.text.alignment.horizontal = HorizontalAlign::Center;
-	ui_upper_right.text.alignment.horizontal  = HorizontalAlign::Right;
-	ui_lower_right.text.alignment.horizontal  = HorizontalAlign::Right;
-
-	//レイアウト用の隠しフレームを作る
-	let per100 = Val::Percent( 100.0 );
-	let center_frame = hidden_frame( Style
-	{	size           : Size::new( per100, per100 ),
-		position_type  : PositionType::Absolute,
-		justify_content: JustifyContent::Center,
-		align_items    : AlignItems::Center,
-		..Default::default()
-	} );
-	let upper_frame  = hidden_frame( Style
-	{	size           : Size::new( Val::Px( SCREEN_WIDTH ), Val::Px( SCREEN_HEIGHT ) ),
-		position_type  : PositionType::Absolute,
-		flex_direction : FlexDirection::Column,
-		justify_content: JustifyContent::FlexEnd, //画面の上端
-		..Default::default()
-	} );
-	let lower_frame  = hidden_frame( Style
-	{	size           : Size::new( Val::Px( SCREEN_WIDTH ), Val::Px( SCREEN_HEIGHT ) ),
-		position_type  : PositionType::Absolute,
-		flex_direction : FlexDirection::Column,
-		justify_content: JustifyContent::FlexStart, //画面の下端
-		..Default::default()
-	} );
-
-	//隠しフレームの上に子要素を作成する
-	let timer = Timer::from_seconds( 1.0, false );
-	cmds.spawn_bundle( center_frame ).with_children( | cmds |
-	{	cmds.spawn_bundle( demo_text  ).insert( MessageDemo  );
-		cmds.spawn_bundle( start_text ).insert( MessageStart { count: 0, timer: timer.clone() } );
-		cmds.spawn_bundle( clear_text ).insert( MessageClear { count: 0, timer: timer.clone() } );
-		cmds.spawn_bundle( over_text  ).insert( MessageOver  { count: 0, timer: timer.clone() } );
-		cmds.spawn_bundle( pause_text ).insert( MessagePause );
-
-		cmds.spawn_bundle( upper_frame ).with_children( | cmds |
-		{	cmds.spawn_bundle( ui_upper_left   ).insert( UiUpperLeft   );
-			cmds.spawn_bundle( ui_upper_center ).insert( UiUpperCenter );
-			cmds.spawn_bundle( ui_upper_right  ).insert( UiUpperRight  );
-		} );
-
-		cmds.spawn_bundle( lower_frame ).with_children( | cmds |
-		{	cmds.spawn_bundle( ui_lower_left   ).insert( UiLowerLeft   );
-			cmds.spawn_bundle( ui_lower_center ).insert( UiLowerCenter );
-			cmds.spawn_bundle( ui_lower_right  ).insert( UiLowerRight  );
-		} );
-	} );
-}
-
-//TextBundleを作る
-fn text_ui( message: &[ MessageSect ], asset_svr: &Res<AssetServer> ) -> TextBundle
-{	let mut sections = Vec::new();
-	for ( line, file, size, color ) in message
-	{	let value = line.to_string();
-		let style = TextStyle
-		{	font     : asset_svr.load( *file ),
-			font_size: *size,
-			color    : *color
-		};
-		sections.push( TextSection { value, style } );
-	}
-
-	let alignment = TextAlignment
-	{	vertical  : VerticalAlign::Center,
-		horizontal: HorizontalAlign::Center,
-	};
-	let position_type = PositionType::Absolute;
-
-	let text  = Text { sections, alignment };
-	let style = Style { position_type, ..Default::default() };
-
-	TextBundle { style, text, ..Default::default() }
-}
-
-//レイアウト用に隠しフレームを作る
-fn hidden_frame( style: Style ) -> NodeBundle
-{	let visibility = Visibility { is_visible: false };
-
-	NodeBundle { style, visibility, ..Default::default() }
-}
-*/
 //End of code.
--- src/ui.rs
+++ src/ui.rs
@@ -37,49 +37,76 @@ impl Plugin for PluginUi
 //テキストUIを配置する
 fn spawn_text_ui_message( mut cmds: Commands, asset_svr: Res<AssetServer> )
 {	//中央に表示するtext
-	let mut pause_text = text_messsage( &MESSAGE_PAUSE, &asset_svr );
-	let mut clear_text = text_messsage( &MESSAGE_CLEAR, &asset_svr );
-	let mut event_text = text_messsage( &MESSAGE_EVENT, &asset_svr );
-	pause_text.visibility.is_visible = false;	//初期は非表示
-	clear_text.visibility.is_visible = false;	//初期は非表示
-	event_text.visibility.is_visible = false;	//初期は非表示
+	let mut pause_text = text_ui( &MESSAGE_PAUSE, &asset_svr );
+	let mut clear_text = text_ui( &MESSAGE_CLEAR, &asset_svr );
+	let mut event_text = text_ui( &MESSAGE_EVENT, &asset_svr );
+	pause_text.visibility.is_visible = false;
+	clear_text.visibility.is_visible = false;
+	event_text.visibility.is_visible = false;
 
-	//上端に表示するtext
-	let mut ui_upper_right = text_messsage( &UI_UPPER_RIGHT, &asset_svr );
+	//上端・下端に表示するtext
+	let mut ui_upper_right = text_ui( &UI_UPPER_RIGHT, &asset_svr );
+	let mut ui_upper_left  = text_ui( &UI_UPPER_LEFT , &asset_svr );
+	let mut ui_lower_left  = text_ui( &UI_LOWER_LEFT , &asset_svr );
+
 	ui_upper_right.style.align_self = AlignSelf::FlexEnd;
-	ui_upper_right.text.alignment.horizontal = HorizontalAlign::Right;
-	let mut ui_upper_left = text_messsage( &UI_UPPER_LEFT, &asset_svr );
-	ui_upper_left.style.align_self = AlignSelf::FlexStart;
-	ui_upper_left.text.alignment.horizontal = HorizontalAlign::Left;
+	ui_upper_left.style.align_self  = AlignSelf::FlexStart;
+	ui_lower_left.style.align_self  = AlignSelf::FlexStart;
 
-	//下端に表示するtext
-	let mut ui_lower_left = text_messsage( &UI_LOWER_LEFT, &asset_svr );
-	ui_lower_left.style.align_self = AlignSelf::FlexStart;
-	ui_lower_left.text.alignment.horizontal = HorizontalAlign::Left;
+	ui_upper_right.text.alignment.horizontal = HorizontalAlign::Right;
+	ui_upper_left.text.alignment.horizontal  = HorizontalAlign::Left;
+	ui_lower_left.text.alignment.horizontal  = HorizontalAlign::Left;
+
+	//レイアウト用の隠しフレームを作る
+	let per100 = Val::Percent( 100.0 );
+	let center_frame = hidden_frame( Style
+	{	size           : Size::new( per100, per100 ),
+		position_type  : PositionType::Absolute,
+		justify_content: JustifyContent::Center,
+		align_items    : AlignItems::Center,
+		..Default::default()
+	} );
+	let upper_frame = hidden_frame( Style
+	{	size           : Size::new( Val::Px( SCREEN_WIDTH ), Val::Px( SCREEN_HEIGHT ) ),
+		position_type  : PositionType::Absolute,
+		flex_direction : FlexDirection::Column,
+		justify_content: JustifyContent::FlexEnd, //画面の上端
+		..Default::default()
+	} );
+	let lower_frame = hidden_frame( Style
+	{	size           : Size::new( Val::Px( SCREEN_WIDTH ), Val::Px( SCREEN_HEIGHT ) ),
+		position_type  : PositionType::Absolute,
+		flex_direction : FlexDirection::Column,
+		justify_content: JustifyContent::FlexStart, //画面の下端
+		..Default::default()
+	} );
 
 	//隠しフレームの上に子要素を作成する
-	cmds.spawn_bundle( hidden_frame_for_centering() ).with_children( | cmds |
+	cmds.spawn_bundle( center_frame ).with_children( | cmds |
 	{	cmds.spawn_bundle( pause_text ).insert( MessagePause );
 		cmds.spawn_bundle( clear_text ).insert( MessageClear );
 		cmds.spawn_bundle( event_text ).insert( MessageEvent );
 
-		cmds.spawn_bundle( hidden_upper_frame() ).with_children( | cmds |
-		{	cmds.spawn_bundle( ui_upper_right ).insert( UiUpperRight );
-			cmds.spawn_bundle( ui_upper_left  ).insert( UiUpperLeft  );
+		cmds.spawn_bundle( upper_frame ).with_children( | cmds |
+		{	cmds.spawn_bundle( ui_upper_left   ).insert( UiUpperLeft   );
+//			cmds.spawn_bundle( ui_upper_center ).insert( UiUpperCenter );
+			cmds.spawn_bundle( ui_upper_right  ).insert( UiUpperRight  );
 		} );
 
-		cmds.spawn_bundle( hidden_lower_frame() ).with_children( | cmds |
-		{	cmds.spawn_bundle( ui_lower_left ).insert( UiLowerLeft );
+		cmds.spawn_bundle( lower_frame ).with_children( | cmds |
+		{	cmds.spawn_bundle( ui_lower_left   ).insert( UiLowerLeft   );
+//			cmds.spawn_bundle( ui_lower_center ).insert( UiLowerCenter );
+//			cmds.spawn_bundle( ui_lower_right  ).insert( UiLowerRight  );
 		} );
 
 	} );
 }
 
 //TextBundleを作る
-fn text_messsage( message: &[ MessageSect ], asset_svr: &Res<AssetServer> ) -> TextBundle
+fn text_ui( message: &[ MessageSect ], asset_svr: &Res<AssetServer> ) -> TextBundle
 {	let mut sections = Vec::new();
-	for ( value, file, size, color ) in message.iter()
-	{	let value = value.to_string();
+	for ( line, file, size, color ) in message
+	{	let value = line.to_string();
 		let style = TextStyle
 		{	font     : asset_svr.load( *file ),
 			font_size: *size,
@@ -87,53 +114,22 @@ fn text_messsage( message: &[ MessageSect ], asset_svr: &Res<AssetServer> ) -> T
 		};
 		sections.push( TextSection { value, style } );
 	}
-	let alignment = TextAlignment { vertical: VerticalAlign::Center, horizontal: HorizontalAlign::Center };
-	let text = Text { sections, alignment };
-	let style = Style { position_type: PositionType::Absolute, ..Default::default() };
-	TextBundle { style, text, ..Default::default() }
+
+	let alignment = TextAlignment
+	{	vertical  : VerticalAlign::Center,
+		horizontal: HorizontalAlign::Center,
+	};
+	let position_type = PositionType::Absolute;
+
+	let text  = Text { sections, alignment };
+	let style = Style { position_type, ..Default::default() };
+	TextBundle { text, style, ..Default::default() }
 }
 
-//中央寄せ用の隠しフレーム
-fn hidden_frame_for_centering() -> NodeBundle
-{	let per100 = Val::Percent( 100.0 );
-	let style = Style
-	{	size: Size::new( per100, per100 ),
-		position_type  : PositionType::Absolute,
-		justify_content: JustifyContent::Center,
-		align_items    : AlignItems::Center,
-		..Default::default()
-	};
-	let visibility = Visibility { is_visible: false, ..Default::default() };
-	NodeBundle { style, visibility, ..Default::default() }
-}
+//レイアウト用に隠しフレームを作る
+fn hidden_frame( style: Style ) -> NodeBundle
+{	let visibility = Visibility { is_visible: false };
 
-//上端幅合せ用の隠しフレーム
-fn hidden_upper_frame() -> NodeBundle
-{	let width  = Val::Px( SCREEN_WIDTH  );
-	let height = Val::Px( SCREEN_HEIGHT );
-	let style = Style
-	{	size: Size::new( width, height ),
-		position_type  : PositionType::Absolute,
-		flex_direction : FlexDirection::Column,
-		justify_content: JustifyContent::FlexEnd, //画面の上端
-		..Default::default()
-	};
-	let visibility = Visibility { is_visible: false, ..Default::default() };
-	NodeBundle { style, visibility, ..Default::default() }
-}
-
-//下端幅合せ用の隠しフレーム
-fn hidden_lower_frame() -> NodeBundle
-{	let width  = Val::Px( SCREEN_WIDTH  );
-	let height = Val::Px( SCREEN_HEIGHT );
-	let style = Style
-	{	size: Size::new( width, height ),
-		position_type  : PositionType::Absolute,
-		flex_direction : FlexDirection::Column,
-		justify_content: JustifyContent::FlexStart, //画面の下端
-		..Default::default()
-	};
-	let visibility = Visibility { is_visible: false, ..Default::default() };
 	NodeBundle { style, visibility, ..Default::default() }
 }
 
@@ -150,7 +146,7 @@ fn spawn_hp_gauge_sprite( mut cmds: Commands )
 	};
 	let transform = Transform::from_translation( Vec3::new( x, y, GAUGE_DEPTH ) );
 
-	let bundle = SpriteBundle { transform, sprite, ..Default::default() };
+	let bundle = SpriteBundle { sprite, transform, ..Default::default() };
 	cmds.spawn_bundle( bundle ).insert( HpGauge );
 }
 
@@ -192,8 +188,6 @@ fn update_ui_upper_left
 		ui.sections[ 1 ].value = hp_gauge;
 	}
 }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //下端の情報表示を更新する(左)
 fn update_ui_lower_left
